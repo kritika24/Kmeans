97184f83e03ddd97b7dc18b60c3a964b9d456e40
KRITIKA GUPTA <kg9810@gmail.com>
2018-06-12 10:32:24 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -39,6 +39,12 @@ pred = clf.predict(vectors_test)
 metrics.f1_score(test.target, pred, average='weighted')
 
 
+'''#Filtering text
+newsgroups_test = fetch_20newsgroups(subset='test',remove=('headers', 'footers', 'quotes'),categories=categories)
+vectors_test = vectorizer.transform(newsgroups_test.data)
+pred = clf.predict(vectors_test)
+metrics.f1_score(pred, newsgroups_test.target, average='weighted')'''
+
 def show_top10(classifier, vectorizer, categories):
     feature_names = np.asarray(vectorizer.get_feature_names())
     for i, category in enumerate(categories):
