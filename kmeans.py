720bef7d52b0b1dc6076c29a75a75f842e5dbd59
KRITIKA GUPTA <kg9810@gmail.com>
2018-06-12 10:29:28 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -10,3 +10,9 @@ import pprint as pprint
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn import metrics
+#converting the data into train and test
+train = fetch_20newsgroups(subset='train')
+test = fetch_20newsgroups(subset='test')
+pprint.pprint(test)
+names=list[train.target_names]
+labels=train.target
